ea592009c105e75f91f0184c2ff190f31b4b4edd
Anna Huang <annahuang@Annas-MacBook-Pro.local>
2024-10-18 16:37:36 -0500

    added branchcnd

--- project/demo1/verilog/execute.v
+++ project/demo1/verilog/execute.v
@@ -5,16 +5,18 @@
    Description     : This is the overall module for the execute stage of the processor.
 */
 `default_nettype none
-module execute (RSData, RTData, PC, Imm5, Imm8, sImm8, sImm11, BSrc, ImmSrc, ALUJmp, invA, invB, ALUSign, cin, ALU_Out, PC_Next);
+module execute (RSData, RTData, PC, Imm5, Imm8, sImm8, sImm11, BSrc, ImmSrc, ALUJmp, invA, invB, ALUSign, cin, BranchTaken, ALU_Out, PC_Next);
    input [15:0] RSData, RTData, PC;
    input [15:0] Imm5, Imm8, sImm8, sImm11;
    input [1:0] BSrc;
+   input [3:0] BranchTaken;
    input ImmSrc, ALUJmp, invA, invB, ALUSign, cin;
    output [15:0] ALU_Out, PC_Next;
 
    wire [15:0] ALUIn;
    wire [15:0] PC_I, PC_Branch, Branch;
    wire BrchCnd;
+   wire SF, CF, OF, ZF;
 
    // PC Adder
    assign PC_I = (ImmSrc) ? sImm8 : sImm11;
@@ -28,9 +30,10 @@ module execute (RSData, RTData, PC, Imm5, Imm8, sImm8, sImm11, BSrc, ImmSrc, ALU
    assign ALUIn = (BSrc == 2'b00) ? RTData : (BSrc == 2'b01) ? Imm5 : (BSrc == 2'b10) ? Imm5 : 16'h0000;
    
    // Register Adder NOT DONE NEED TO FIX ALU
-   alu alu1(.InA(RSData), .InB(ALUIn), .Cin(cin), .Oper(), .invA(invA), .invB(invB), .sign(ALUSign), .Out(ALU_Out), .Zero(), .Ofl());
+   alu alu1(.InA(RSData), .InB(ALUIn), .Cin(cin), .Oper(), .invA(invA), .invB(invB), .sign(ALUSign), .Out(ALU_Out), .Zero(ZF), .SF(SF), .Ofl(OF), .CF(CF));
    
-   //BrchCnd NEED TO DO
+   //BrchCnd 
+   brchcnd branch_ctrl(.SF(SF), .ZF(ZF), .brch_instr(BranchTaken), .BrchCnd(BrchCnd));
 
 endmodule
 `default_nettype wire

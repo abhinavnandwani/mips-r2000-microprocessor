b58e9bf3b3f2753401b91829f0a76e54f07002dc
Abhinav Nandwani <nandwani2@wisc.edu>
2024-10-18 15:58:12 -0500

    alu control

--- project/demo1/verilog/alu_control.v
+++ project/demo1/verilog/alu_control.v
@@ -6,10 +6,21 @@ module alu_control(aluoper,instr,op);
     output [3:0] op;
 
     always@(*) 
-        case(aluoper)
-          4'b1100 : op = 4'b1000;
-          4'b1101 : op = 4'b1001;
-          4'b1110 : op = 4'b1010;
-          4'b1111 : op = 4'b1011; 
+        case(aluoper[5:3])
+        
+        4'b000: op = {0,aluoper[2:0]}; // I-type 1 //
+        4'b001: op = 4'b0000;  // I-type 2 //
+        4'b010: op = {0,aluoper[0],instr}; // R-type //
+
+        // set if operations //
+        4'b011 : op = 4'b1000;
+        4'b100 : op = 4'b1001;
+        4'b101 : op = 4'b1010;
+        4'b110 : op = 4'b1011;
+
+        4'b111 : op = 4'b1100; //BTR
+        endcase
+endmodule
+         
 
     

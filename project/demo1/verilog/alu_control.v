6f5347469cc1963a959460778ab05b056499ef14
Abhinav Nandwani <nandwani2@wisc.edu>
2024-10-18 16:32:20 -0500

    alu

--- project/demo1/verilog/alu_control.v
+++ project/demo1/verilog/alu_control.v
@@ -1,24 +1,24 @@
 module alu_control(aluoper,instr,op);
 
     input [1:0] instr;
-    input [3:0] aluoper;
+    input [5:0] aluoper;
 
-    output [3:0] op;
+    output reg [3:0] op;
 
     always@(*) 
         case(aluoper[5:3])
         
-        4'b000: op = {0,aluoper[2:0]}; // I-type 1 //
-        4'b001: op = 4'b0000;  // I-type 2 //
-        4'b010: op = {0,aluoper[0],instr}; // R-type //
+        3'b000: op = {1'b0,aluoper[2:0]}; // I-type 1 //
+        3'b001: op = 4'b0000;  // I-type 2 TO DO//
+        3'b010: op = {1'b0,aluoper[0],instr}; // R-type //
 
         // set if operations //
-        4'b011 : op = 4'b1000;
-        4'b100 : op = 4'b1001;
-        4'b101 : op = 4'b1010;
-        4'b110 : op = 4'b1011;
+        3'b011 : op = 4'b1000;
+        3'b100 : op = 4'b1001;
+        3'b101 : op = 4'b1010;
+        3'b110 : op = 4'b1011;
 
-        4'b111 : op = 4'b1100; //BTR
+        3'b111 : op = 4'b1100; //BTR
         endcase
 endmodule
          

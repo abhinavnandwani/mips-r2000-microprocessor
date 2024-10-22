5dff7ca2b4511ea9f358a9c827711e0e92390f60
Abhinav Nandwani <abhinav@Abhinavs-MacBook-Pro.local>
2024-10-22 10:08:31 -0500

    demo1_submission

---
+++ project/demo1_submission/verilog/fetch.v
@@ -0,0 +1,36 @@
+/* 
+    Author          : Abhinav Nandwani, Anna Huang
+    Filename        : fetch.v
+    Description     : This is the overall module for the fetch state of the processor. 
+*/
+
+module fetch (clk, rst, PC_B,nHaltSig, instr, PC_Next,PC_curr);
+   input clk, rst;
+   input [15:0] PC_B; // The PC value "back" from the execute stage. 
+   input nHaltSig;
+   output [15:0] instr;
+   output [15:0] PC_Next,PC_curr;
+
+
+   wire [15:0] PC;
+   wire err;
+   wire [15:0] add2,PC_Sum;
+   wire c_out;
+
+   // PC Register
+   
+   register pc_reg (.r(PC), .w(PC_B), .clk(clk), .rst(rst), .we(1'b1));
+   assign PC_curr = PC;
+
+   // Instruction Memory
+   memory2c instr_mem(.data_out(instr), .data_in(16'h0000), .addr(PC), .enable(1'b1), .wr(1'b0), .createdump(~nHaltSig), .clk(clk), .rst(rst));
+
+   // Adder: PC + 2
+
+   cla_16b pc_add2 (.sum(PC_Sum), .c_out(c_out), .a(PC), .b(16'h0002), .c_in(1'b0));
+   
+   // Halt Mux
+   assign PC_Next =  (!nHaltSig) ? PC:PC_Sum;
+ 
+
+endmodule

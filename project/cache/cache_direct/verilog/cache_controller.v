a2ae23fb37e211c8fecbcdd8c03c3e24b496fc40
abhinavnandwani <nandwani2@wisc.edu>
2024-11-23 16:49:31 -0600

    fixes in cache controller

--- project/cache/cache_direct/verilog/cache_controller.v
+++ project/cache/cache_direct/verilog/cache_controller.v
@@ -6,37 +6,34 @@ module cache_controller (
    input wire createdump,
    input wire Rd,
    input wire Wr,
-   input wire busy,
+   input wire [3:0] busy,
    input wire valid,
    input wire dirty,
    input wire hit,
+   input wire mem_stall,
    output reg valid_in,
    output reg comp,
    output reg write,
    output reg write_mem,
-   output reg read_mem
+   output reg read_mem,
+   output reg cache_in,
+   output reg mem_in,
+   output reg done
    );
 
    // Define the states using parameters
-   parameter IDLE                  = 4'b0000;
-   parameter COMPARE_READ          = 4'b0001;
-   parameter COMPARE_WRITE         = 4'b0010;
-   parameter MEMORY_READ           = 4'b0011;
-   parameter ACCESS_READ           = 4'b0100;
-   parameter ACCESS_WRITE          = 4'b0110;
+   parameter IDLE                  = 3'b000;
+   parameter COMPARE_READ          = 3'b001;
+   parameter COMPARE_WRITE         = 3'b010;
+   parameter MEMORY_READ_MISS      = 3'b011;
+   parameter MEMORY_READ_NOTVALID  = 3'b100;
+   parameter ACCESS_READ           = 3'b101;
+   parameter ACCESS_WRITE          = 3'b110;
+
 
    wire [2:0] state;
    reg [2:0] next_state;
 
-   //  // State transition logic
-   //  always @(posedge clk or posedge rst) begin
-   //      if (rst) begin
-   //          state <= IDLE;
-   //          done <= 1'b0;
-   //      end else begin
-   //          state <= next_state;
-   //      end
-   //  end
    dff state_1 [2:0](.q(state), .d(next_state), .clk(clk), .rst(rst));
 
    // Next state and output logic
@@ -47,95 +44,110 @@ module cache_controller (
       write_mem = 1'b0;
       read_mem = 1'b0;
       valid_in = 1'b0;
+      cache_in = 1'b0;
+      mem_in = 1'b0;
+      done = 1'b0;
       next_state = state;
 
       case (state)
          IDLE: begin
             if (Rd) begin
+               comp = 1'b1;
                next_state = COMPARE_READ;
             end else if (Wr) begin 
+               comp = 1'b1;
                next_state = COMPARE_WRITE;
             end
          end
 
          COMPARE_READ: begin
-            /*
-            This case is used when the processor executes a load instruction. 
-            The "tag_in", "index", and "offset" signals need to be valid. 
-            Either a hit or a miss will occur, as indicated by the "hit" output during the same cycle. 
-            If a hit occurs, "data_out" will contain the data and "valid" will indicate if the data is valid. 
-            If a miss occurs, the "valid" output will indicate whether the block occupying that line of the cache is valid. 
-            The "dirty" output indicates the state of the dirty bit in the cache line.
-            */
-            comp = 1'b1;
-            write = 1'b0;
-            if (~valid) next_state = MEMORY_READ_MISS;
-            else begin
-               if (hit)
+            if (~valid) begin
+               comp = 1'b0;
+               write = 1'b0;
+               read_mem = 1'b1;
+               next_state = MEMORY_READ_MISS;
+            end else begin
+               if (hit) begin
+                  done = 1'b1;
                   next_state = IDLE; //data ready
-               else next_state = MEMORY_READ_NOTVALID;
-            end
-
-         end
-
-         MEMORY_READ_MISS : begin // understand 4 banked
-            //when memory ready
-                           comp = 1'b0;
-            valid_in = 1'b1;
-            read_mem = 1'b1;
-            if (~busy) begin
-               if (~valid) begin
-                  next_state = ACCESS_WRITE;
                end else begin
-                  if (dirty) begin
-                     next_state = ACCESS_READ;
-                  end
+                  comp = 1'b0;
+                  write = 1'b0;
+                  read_mem = 1'b1;
+                  next_state = MEMORY_READ_NOTVALID;
                end
             end
          end
 
+         MEMORY_READ_MISS : begin // understand 4 banked (evict)
+            if (~mem_stall) begin
+               if (dirty) begin             // evict, check dirty bit
+                  comp = 1'b0;
+                  write = 1'b0;
+                  mem_in = 1'b1;
+                  write_mem = 1'b1;
+                  next_state = ACCESS_READ;
+               end else begin
+                  comp = 1'b0;
+                  write = 1'b1;
+                  valid_in = 1'b1;
+                  cache_in = 1'b1;
+                  done = 1'b1;
+                  next_state =  ACCESS_WRITE;
+               end
+            end
+         end
+
+         MEMORY_READ_NOTVALID: begin
+            if (~mem_stall) begin
+               comp = 1'b0;
+               write = 1'b1;
+               valid_in = 1'b1;  // set valid bit
+               cache_in = 1'b1;
+               done = 1'b1;
+               next_state = ACCESS_WRITE; // write to cache
+            end
+         end
 
          COMPARE_WRITE: begin
-            comp = 1'b1;
-            write = 1'b1;
-            if (hit && valid) begin
-               if (busy) begin
-                  next_state = COMPARE_WRITE;
+            if (~valid) 
+               if (dirty) begin             // evict, check dirty bit
+                  comp = 1'b0;
+                  write = 1'b0;
+                  mem_in = 1'b1;
+                  write_mem = 1'b1;
+                  next_state = ACCESS_READ;
                end else begin
+                  comp = 1'b0;
+                  write = 1'b1;
+                  valid_in = 1'b1;
+                  cache_in = 1'b0;
+                  done = 1'b1;
                   next_state = ACCESS_WRITE;
                end
-            end else begin
-               next_state = IDLE;
-            end
-         end
-
-         MEMORY_READ: begin
-            comp = 1'b0;
-            valid_in = 1'b1;
-            read_mem = 1'b1;
-            if (~busy) begin
-               if (~valid) begin
-                  next_state = ACCESS_WRITE;
-               end else begin
-                  if (dirty) begin
-                     next_state = ACCESS_READ;
-                  end
-               end
+            else begin
+               if (hit)
+                  comp = 1'b0;
+                  write = 1'b1;
+                  valid_in = 1'b1;
+                  cache_in = 1'b0;
+                  done = 1'b1;
+                  next_state = IDLE;
             end
          end
 
          ACCESS_READ: begin
-            comp = 1'b0;
-            write = 1'b0;
-            write_mem = 1'b1;
-            if (~busy) begin
-               next_state = ACCESS_WRITE;
-            end
+            if (~mem_stall) begin
+               comp = 1'b0;
+               write = 1'b1;
+               valid_in = 1'b1;
+               cache_in = 1'b1;
+               done = 1'b1;
+              next_state = ACCESS_WRITE;
+            end 
          end
 
          ACCESS_WRITE: begin
-            comp = 1'b0;
-            write = 1'b1;
             next_state = IDLE;
          end
 
--- project/cache/cache_direct/verilog/mem_system.v
+++ project/cache/cache_direct/verilog/mem_system.v
@@ -19,23 +19,25 @@ module mem_system(/*AUTOARG*/
    input wire        rst;
    
    output wire [15:0] DataOut;
-   output reg        Done;
+   output wire        Done;
    output wire       Stall;
-   output reg        CacheHit;
+   output wire       CacheHit;
    output wire       err;
 
    wire comp, write;
-   wire hit, dirty, valid, valid_in, busy;
+   wire hit, dirty, valid, valid_in;
+   wire [3:0] busy;
    wire err_mem, err_cache;
-   wire [15:0] data_out;
+   wire [15:0] data_out_cache,data_out_mem;
    wire write_mem, read_mem;
+   wire cache_in, mem_in, mem_stall;
 
    /* data_mem = 1, inst_mem = 0 *
     * needed for cache parameter */
    parameter memtype = 0;
    cache #(0 + memtype) c0(// Outputs
                           .tag_out              (),
-                          .data_out             (data_out),
+                          .data_out             (data_out_cache),
                           .hit                  (hit),
                           .dirty                (dirty),
                           .valid                (valid),
@@ -48,14 +50,14 @@ module mem_system(/*AUTOARG*/
                           .tag_in               (Addr[15:11]),
                           .index                (Addr[10:3]),
                           .offset               (Addr[2:0]),
-                          .data_in              (DataOut),
+                          .data_in              (cache_in ? data_out_mem : DataIn),
                           .comp                 (comp),
                           .write                (write),
                           .valid_in             (valid_in));
 
    four_bank_mem mem(// Outputs
-                     .data_out          (DataOut),
-                     .stall             (Stall),
+                     .data_out          (data_out_mem),
+                     .stall             (mem_stall),
                      .busy              (busy),
                      .err               (err_mem),
                      // Inputs
@@ -63,9 +65,9 @@ module mem_system(/*AUTOARG*/
                      .rst               (rst),
                      .createdump        (createdump),
                      .addr              (Addr),
-                     .data_in           (data_out),
-                     .wr                (Wr),
-                     .rd                (Rd));
+                     .data_in           (mem_in ? data_out_cache : DataIn),
+                     .wr                (write_mem),
+                     .rd                (read_mem));
    
    // your code here
    cache_controller ctrl(
@@ -75,6 +77,7 @@ module mem_system(/*AUTOARG*/
       .Rd(Rd),
       .Wr(Wr),
       .busy(busy),
+      .mem_stall(mem_stall),
       .valid(valid),
       .dirty(dirty),
       .hit(hit),
@@ -82,11 +85,16 @@ module mem_system(/*AUTOARG*/
       .comp(comp),
       .write(write),
       .write_mem(write_mem),
-      .read_mem(read_mem)
+      .read_mem(read_mem),
+      .cache_in(cache_in),
+      .mem_in(mem_in),
+      .done(Done)
    ); 
 
    assign err = err_mem | err_cache;
    assign CacheHit = hit;
+   assign Stall = ~Done;
+   assign DataOut = data_out_cache;
 
 endmodule // mem_system
 `default_nettype wire
